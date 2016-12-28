940e2e732247b70ee9213c00a50483cc776bc8cd
Kaylee Alvarado <kaymay1000@gmail.com>
2016-12-27 20:24:28 -0800

    troubleshooting

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -9,27 +9,22 @@
   reposObj.requestRepos = function(callback) {
     // NOTE: refactor this request into an $.ajax call
     $.when(
-     $.ajax({
-       url: '/github.com/user/repos' +
-            '?per_page=10' +
-            '&sort=updated',
-       type: 'GET',
-       success: function(data) {
-         // NOTE: since the 'data' paramter comes back as an
-         // array of objects, we can reassign allRepos below.
-         reposObj.allRepos = data;
-       }
-     }),
-     $.ajax({
-       url: '/github.com/users/patci/followers' +
-            '?per_page=5' +
-            '&sort=updated',
-       type: 'GET',
-       success: function(data) {
-         reposObj.followers = data;
-       }
-     })
-    ).done(callback);
+      $.get('/github.com/user/repos' +
+      '?per_page=10' +
+      '&sort=updated')
+      .then(function(data) {
+        // NOTE: since the 'data' paramter comes back as an
+        // array of objects, we can reassign allRepos below.
+        reposObj.allRepos = data;
+      })
+    ),
+    $.get('/github.com/users/patci/followers' +
+    '?per_page=5' +
+    '&sort=updated')
+    .then(function(data) {
+      reposObj.followers = data;
+    })
+    .done(callback);
   };
 
   reposObj.withTheAttribute = function(attr) {
