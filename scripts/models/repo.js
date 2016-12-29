a87b340a201e732d0239570c9d70cce8d0657539
Dana Kulp <dkulp23@gmail.com>
2016-12-28 17:50:35 -0800

    Refactored ajax calls for repos and followers using routes defined by proxyServer.

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -9,29 +9,13 @@
   reposObj.requestRepos = function(callback) {
     // NOTE: refactor this request into an $.ajax call
     $.when(
-     $.ajax({
-       url: 'https://api.github.com/user/repos' +
-            '?per_page=10' +
-            '&sort=updated',
-       type: 'GET',
-       headers: { 'Authorization': 'token ' + githubToken },
-       success: function(data) {
-         // NOTE: since the 'data' paramter comes back as an
-         // array of objects, we can reassign allRepos below.
-         reposObj.allRepos = data;
-       }
-     }),
-     $.ajax({
-       url: 'https://api.github.com/users/patci/followers' +
-            '?per_page=5' +
-            '&sort=updated',
-       type: 'GET',
-       headers: { 'Authorization': 'token ' + githubToken },
-       success: function(data) {
-         reposObj.followers = data;
-       }
-     })
-    ).done(callback);
+      $.get('/github/user/repos?type=owner', function(data) {
+        reposObj.allRepos = data;
+      }),
+      $.get('/github/user/followers', function(data) {
+        reposObj.followers = data;
+      })
+    ).done(callback)
   };
 
   reposObj.withTheAttribute = function(attr) {
