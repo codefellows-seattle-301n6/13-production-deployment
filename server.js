aecfa799b782462e4cd587a44a19a65f06b67d6a
Dana Kulp <dkulp23@gmail.com>
2016-12-28 17:35:46 -0800

    Wrote script for proxy and commited github token to environment.

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 'use strict';
 
-var express = require('express'),
+var express = require('express');
   // NOTE: require in our request proxy module
-
-  port = process.env.PORT || 3000,
-  app = express();
+var requestProxy = require('express-request-proxy');
+var port = process.env.PORT || 3000;
+var app = express();
 
 // NOTE: now use our proxy within a function to request
 //        our github data on the server.
 
 
 app.use(express.static('./'));
+
+app.get('/github/*', function(request, response) {
+  console.log(`Routing Github request for ${request.params[0]}`);
+  requestProxy({
+    url: `https://api.github.com/${request.params[0]}`,
+    headers: {
+      Authorization: `token ${process.env.GITHUB_TOKEN}`
+    }
+  })(request, response)
+})
 
 app.get('*', function(request, response) {
   console.log('New request:', request.url);
