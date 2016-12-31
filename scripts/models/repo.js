c90e86c5abcd9e2f7062774a3b611b6343c05361
Dana Kulp <dkulp23@gmail.com>
2016-12-31 10:06:28 -0800

    Refactored script in server and proxy appears to be handling request properly now.

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -9,14 +9,27 @@
   reposObj.requestRepos = function(callback) {
     // NOTE: refactor this request into an $.ajax call
     $.when(
-      $.get('/github/user/repos?type=owner', function(data) {
+      $.get('/github/user/repos?type=owner')
+      .then(function(data) {
         reposObj.allRepos = data;
       }),
-      $.get('/github/user/followers', function(data) {
+      $.get('/github/user/followers')
+      .then(function(data) {
         reposObj.followers = data;
       })
     ).done(callback)
   };
+
+  // reposObj.requestRepos = function(callback) {
+  //   let reqOne = $.get('/github/user/repos?type=owner')
+  //   let reqTwo = $.get('/github/user/followers')
+  //
+  //   $.when(reqOne, reqTwo).done((resOne, resTwo) => {
+  //     reposObj.allRepos = resOne[0]
+  //     reposObj.followers = resTwo[0]
+  //     if (callback) callback()
+  //   })
+  // };
 
   reposObj.withTheAttribute = function(attr) {
     return reposObj.allRepos.filter(function(aRepo) {
